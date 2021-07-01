655dfa7b0abdb8be507a3d68d885f9a8a3eb86d2
Kayce Williams <kalewill88@gmail.com>
2021-07-01 10:50:01 -0400

    Fixed API upon crash

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const apiUrl = 'https://cat-fact.herokuapp.com/facts';
+const apiUrl = 'https://catfact.ninja/fact';
 
-setInterval(getAPI, 3000);
+// setInterval(getAPI, 3000);
 
 async function getAPI() {
     const response = await fetch(apiUrl);
     const resp = await response.json();
+    console.log(resp)
     let count = Object.keys(resp).length;
-    let random = resp[Math.floor(Math.random() * count)].text;
+    // let random = resp[Math.floor(Math.random() * count)].text;
+    let random = resp.fact
     document.getElementById('scrollText').innerHTML = random;
 }
+getAPI()
 
 
 
